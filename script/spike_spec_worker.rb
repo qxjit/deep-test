33ff753245277d80df1bdc605a2f21f51d5f94f5
qxjit <qxjit@376b3db6-7cd3-4bf6-a458-7e8161226db8>
2007-09-17 02:11:04 +0000

    spike for rspec support
    git-svn-id: http://deep-test.rubyforge.org/svn/trunk@21 376b3db6-7cd3-4bf6-a458-7e8161226db8

--- script/spike_spec_worker.rb
+++ script/spike_spec_worker.rb
@@ -1,6 +1,64 @@
 require 'rubygems'
 require 'deep_test/server'
 require 'deep_test/tuple_space_factory'
+require 'spec'
+
+class Runner
+  def initialize
+    @behaviours = []
+  end
+
+  def add_behaviour(behaviour)
+    @behaviours << behaviour
+  end
+
+  def run(from, reporter, dry_run, timeout)
+    b = @behaviours.find {|b| b.has?(from)}
+    b.run_one_example(from, reporter, dry_run, timeout)
+  end
+end
+
+class Reporter
+  attr_reader :result
+
+  def example_started(name)
+  end
+  
+  def example_finished(name, error=nil, failure_location=nil, not_implemented = false)
+    @result = [name.from, error, failure_location, not_implemented]
+  end
+end
+
+$behaviour_runner = Runner.new
+
+module Spec
+  module DSL
+    class Behaviour
+      def has?(from)
+        examples.any? {|e| e.from == from}
+      end
+
+      def run_one_example(from, reporter, dry_run=false, timeout=nil)
+        prepare_execution_context_class
+        example = examples.find {|e| e.from == from}
+        example_execution_context = execution_context(example)
+        befores = before_each_proc(behaviour_type) {|e| raise e}
+        afters = after_each_proc(behaviour_type)
+        example.run(reporter, befores, afters, dry_run, example_execution_context, timeout)
+      end
+    end
+  end
+end
+
+module Spec
+  module DSL
+    class Example
+      attr_reader :from
+    end
+  end
+end
+
+require File.dirname(__FILE__) + '/../spec/example_spec'
 
 module DeepTest
   class SpecWorker
@@ -10,8 +68,13 @@ module DeepTest
     
     def run
       loop do
-        tuple = @tuple_space.take ["run_test", nil, nil], 30
-        p tuple
+        tuple = @tuple_space.take ["run_spec", nil], 30
+        example_from = tuple[1]
+        r = Reporter.new
+        $behaviour_runner.run(example_from, r, false, 30)
+        tuple = ["spec_result"].concat(r.result)
+        puts "writing #{tuple.inspect}"
+        @tuple_space.write tuple
       end
     end
   end
