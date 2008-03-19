59f816eadaf0cc6af8eb0fa19ab068deafc6972b
smtlaissezfaire <smtlaissezfaire@376b3db6-7cd3-4bf6-a458-7e8161226db8>
2008-03-19 06:13:04 +0000

    * Adding tuple space factory specs
    git-svn-id: http://deep-test.rubyforge.org/svn/trunk@144 376b3db6-7cd3-4bf6-a458-7e8161226db8

--- lib/deep_test/tuple_space_factory.rb
+++ lib/deep_test/tuple_space_factory.rb
@@ -6,7 +6,7 @@ module DeepTest
       DRb.start_service
       ts = Rinda::RingFinger.new([Socket.gethostname],options.server_port).lookup_ring_any
       DeepTest.logger.debug "Connected to DeepTest server at #{ts.__drburi}"
-      Rinda::TupleSpaceProxy.new ts      
+      Rinda::TupleSpaceProxy.new ts
     end
   end
 end
