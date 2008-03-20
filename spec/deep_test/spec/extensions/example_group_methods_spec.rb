67b1fb9195e382c45b9f541cb089c0bc7e433e02
smtlaissezfaire <smtlaissezfaire@376b3db6-7cd3-4bf6-a458-7e8161226db8>
2008-03-20 02:32:10 +0000

    Refactoring ExampleGroupMethod Specs
    git-svn-id: http://deep-test.rubyforge.org/svn/trunk@150 376b3db6-7cd3-4bf6-a458-7e8161226db8

--- spec/deep_test/spec/extensions/example_group_methods_spec.rb
+++ spec/deep_test/spec/extensions/example_group_methods_spec.rb
@@ -1,101 +1,109 @@
 require File.dirname(__FILE__) + "/../../../spec_helper"
 
-describe Spec::Example::ExampleGroupMethods do
-  it_should_behave_like "sandboxed rspec_options"
-  it "should raise error on append_before(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        append_before(:all) {}
+module Spec
+  module Example
+    describe ExampleGroupMethods do
+      before :each do
+        @error_class = ExampleGroupMethods::BeforeAfterAllNotSupportedByDeepTestError
       end
-    }.should raise_error
-  end
-
-  it "should not raise error on append_before(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        append_before(:each) {}
+      
+      it_should_behave_like "sandboxed rspec_options"
+      
+      it "should issue a warning on append_before(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            append_before(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
-  it "should raise error on before(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        before(:all) {}
+      it "should not raise error on append_before(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            append_before(:each) {}
+          end
+        }.should_not raise_error
       end
-    }.should raise_error
-  end
 
-  it "should not raise error on before(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        before(:each) {}
+      it "should raise error on before(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            before(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
-  it "should raise error on prepend_before(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        prepend_before(:all) {}
+      it "should not raise error on before(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            before(:each) {}
+          end
+        }.should_not raise_error
       end
-    }.should raise_error
-  end
 
-  it "should not raise error on prepend_before(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        prepend_before(:each) {}
+      it "should raise error on prepend_before(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            prepend_before(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
-  it "should raise error on append_after(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        append_after(:all) {}
+      it "should not raise error on prepend_before(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            prepend_before(:each) {}
+          end
+        }.should_not raise_error
       end
-    }.should raise_error
-  end
 
-  it "should not raise error on append_after(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        append_after(:each) {}
+      it "should raise error on append_after(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            append_after(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
-  it "should raise error on after(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        after(:all) {}
+      it "should not raise error on append_after(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            append_after(:each) {}
+          end
+        }.should_not raise_error
       end
-    }.should raise_error
-  end
 
-  it "should not raise error on after(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        after(:each) {}
+      it "should raise error on after(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            after(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
-  it "should raise error on prepend_after(:all)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        prepend_after(:all) {}
+      it "should not raise error on after(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            after(:each) {}
+          end
+        }.should_not raise_error
       end
-    }.should raise_error
-  end
 
-  it "should not raise error on prepend_after(:each)" do
-    lambda {
-      Class.new(Spec::Example::ExampleGroup) do
-        prepend_after(:each) {}
+      it "should raise error on prepend_after(:all)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            prepend_after(:all) {}
+          end
+        }.should raise_error(@error_class)
       end
-    }.should_not raise_error
-  end
 
+      it "should not raise error on prepend_after(:each)" do
+        lambda {
+          Class.new(Spec::Example::ExampleGroup) do
+            prepend_after(:each) {}
+          end
+        }.should_not raise_error
+      end
+    end
+  end
 end
