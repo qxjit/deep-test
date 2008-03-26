9a3370682dc917eaf8cc07aa55080d0221fbfc9f
qxjit <qxjit@376b3db6-7cd3-4bf6-a458-7e8161226db8>
2008-03-26 21:40:04 +0000

    make RSpec detector nicer about older versions of rspec
    git-svn-id: http://deep-test.rubyforge.org/svn/trunk@194 376b3db6-7cd3-4bf6-a458-7e8161226db8

--- lib/deep_test/rspec_detector.rb
+++ lib/deep_test/rspec_detector.rb
@@ -4,7 +4,20 @@ module DeepTest
       require "rubygems"
       # requiring 'spec' directly blows up unit-record
       require "spec/version" 
-      yield if defined?(::Spec)
+      if defined?(::Spec)
+        if ::Spec::VERSION::MAJOR >= 1 &&
+           ::Spec::VERSION::MINOR >= 1 &&
+           ::Spec::VERSION::TINY  >= 3
+          yield
+        else
+          require 'spec/rake/spectask'
+          ::Spec::Rake::SpecTask.class_eval do
+            def deep_test(options)
+              raise "* DeepTest RSpec support requires RSpec 1.1.3 or greater"
+            end
+          end
+        end
+      end
     rescue LoadError, Gem::LoadError
     end
   end
